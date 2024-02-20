4b0fb3f26f14b9f64703ad06438dc4a7c9e50e1d
Zander Bournand <z.bournand@gmail.com>
2024-02-20 14:20:59 -0500

    fixed status bar

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import AppNavigator from "./utils/AppNavigator";
 import { StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   return (
     <GestureHandlerRootView style={styles.appContainer}>
       <BottomSheetModalProvider>
+        <StatusBar style="auto" />
         <AppNavigator />
       </BottomSheetModalProvider>
     </GestureHandlerRootView>
